71e87fb4a5239db76b981bb58ef34eb1bf9a867d
Gabriel Mochiute <gabriel.almeida@primi.com.br>
2022-07-18 09:49:24 -0300

    feat: First Commit

---
+++ src/main/java/br/com/springboot/model/User.java
@@ -0,0 +1,17 @@
+package br.com.springboot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private Long id;
+	private String name;
+	private String username;
+
+}
